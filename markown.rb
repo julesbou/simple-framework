30c99a639808a26c37a37e66f7d12f1447d19bd2
gordonslondon <jules.boussekeyt@gmail.com>
2011-09-02 21:32:09 +0200

    added a way to parse the documentation

---
+++ markown.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+#  Github-flavored markdown to HTML, in a command-line util.
+#
+#  $ cat README.md | ./ghmarkdown.rb
+#
+#  Notes:
+#
+#  You will need to install Pygments for syntax coloring
+#
+#    $ sudo easy_install pygments
+#
+#  Install the gems redcarpet, albino, and nokogiri
+#
+#  To work with http://markedapp.com/ I also had to
+#    $ sudo ln -s /usr/local/bin/pygmentize /usr/bin
+#
+require 'rubygems'
+require 'redcarpet'
+require 'albino'
+require 'nokogiri'
+
+def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    html = Redcarpet.new(text, *options).to_html 
+syntax_highlighter(html)
+    end
+
+    def syntax_highlighter(html)
+doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s # doc.at_css("body").inner_html.to_s
+    end
+
+puts markdown(ARGF.read)
