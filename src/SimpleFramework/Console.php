add0e9c608f1a1609fb791940684a5778e53e1ba
gordonslondon <jules.boussekeyt@gmail.com>
2011-09-05 22:25:36 +0200

    [Console] added console

---
+++ src/SimpleFramework/Console.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SimpleFramework;
+
+class Console
+{
+    private $kernel;
+    private $commands = array();
+
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function register($name, $command)
+    {
+        $this->commands[$name] = $command;
+    }
+
+    public function run($argv)
+    {
+        if (!isset($argv[1])) {
+            echo "please specify a command:\n";
+            echo '- '.implode("\n- ", array_keys($this->commands));
+            exit;
+        }
+
+        $name = $argv[1];
+
+        if (!isset($this->commands[$name])) {
+            echo "invalid command '{$name}'\n";
+            exit;
+        }
+
+        $command = $this->commands[$name];
+        $args = array_splice($argv, 0, 2);
+
+        $pair = true;
+        $prev;
+        foreach ($argv as $k => $val) {
+            if (!$pair) {
+            $argv[$prev] = $val;
+            } else {
+                $prev = $val;
+            }
+            $pair = !$pair;
+            unset($argv[$k]);
+        }
+
+        if (!method_exists($command, 'run')) {
+            echo "command '{$name}' should have a run() method\n";
+            exit;
+        }
+
+        $command->run($this->kernel->getContainer(), $argv);
+    }
+}
